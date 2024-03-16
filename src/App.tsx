bb35bf5c88e164a07fa487a6c26b6f5e8d09d248
unxok <anderluke707@gmail.com>
2024-03-15 22:30:56 -0700

    testing gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "./components/ui/button";
 function App() {
 	return (
 		<div className='bg-neutral-900 text-white inset-0 fixed flex items-center justify-center gap-3'>
-			Hello world!
+			Hello world!!
 			<Button variant={"destructive"}>Im a button!</Button>
 		</div>
 	);
