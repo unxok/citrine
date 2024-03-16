ac3b635d019c67d798ce7d1b05718e266758b839
unxok <anderluke707@gmail.com>
2024-03-15 22:47:15 -0700

    added base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "https://unxok.github.io/panel-offline/",
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
