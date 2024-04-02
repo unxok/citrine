4975c8cecfcd5b425c9459f1a5ed18d9f38ed08e
unxok <54017499+unxok@users.noreply.github.com>
2024-04-02 15:03:24 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -389,7 +389,7 @@ const SettingsSheet = () => {
     const csv = cards.reduce((acc, card) => {
       const boardTitle = boards.find(b => b.id === card.board)?.title;
       const laneTitle = boards.find(b => b?.lanes?.some(l => l.id === card.lane))?.title;
-      const arr = [card.id, card?.title, card?.description, boardTitle, laneTitle, card?.notes, card?.created, card?.modified]
+      const arr = [`${card.id}`, `"${card?.title}"`, `"${card?.description}"`, `"${boardTitle}"`, `"${laneTitle}"`, `"${card?.notes}"`, `"${card?.created}"`, `"${card?.modified}"`]
       const row = arr.join(delim);
       return acc + '\n' + row;
     }, '')
